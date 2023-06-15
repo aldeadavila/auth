d949f1eb42370cd59d39cee8a28536d3747fc855
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-06-15 23:44:31 +0200

    add sign out button

--- app/src/main/java/com/aldeadavila/auth/MainActivity.kt
+++ app/src/main/java/com/aldeadavila/auth/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.aldeadavila.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aldeadavila.auth.databinding.ActivityMainBinding
+import com.aldeadavila.auth.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignOut.setOnClickListener { signOut() }
+
+
+
+
+    }
+
+    private fun signOut() {
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth.signOut()
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 }
